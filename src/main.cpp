31c191a2006473c1df78ff003bc7d59c7018582e
huang-wt <314936302@qq.com>
2023-01-27 01:51:08 +0000

    add simple interface for data fetching service

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,9 @@
 using namespace std;
 
 
-int main() {
+void displayStaticData() {
+    cout << "\t\t\t -- Static Data --\n\n";
 
-    //---------static----------
     string os = LinuxParser::OperatingSystem();
     cout << "Operation System: " << os << endl;
 
@@ -22,25 +22,94 @@ int main() {
     int cpuNum = LinuxParser::CpuCoresCount();
     cout << "Cores number: " << cpuNum << endl;
 
-    //---------dynamic----------
-    float memUtilization = LinuxParser::MemoryUtilization();
-    cout << "Memory Utilization: " << memUtilization * 100.0 << "%" << endl;
+    cin.get();
+    cin.get();
+}
 
-    int totalProcesses = LinuxParser::TotalProcesses();
-    cout << "Total Processes: " << totalProcesses << endl;
+void displayDynamicData() {
+    cout << "\t\t\t -- Dynamic Data --\n\n";
 
-    int runningProcesses = LinuxParser::RunningProcesses();
-    cout << "Running Processes: " << runningProcesses << endl;
+    for (int i = 0 ; i < 5 ; i++, sleep(1)) {
+        float memUtilization = LinuxParser::MemoryUtilization();
+        cout << "Memory Utilization: " << memUtilization * 100.0 << "%" << endl;
 
-    long upTime = LinuxParser::UpTime();
-    cout << "Up Time: " << upTime << endl;
+        int totalProcesses = LinuxParser::TotalProcesses();
+        cout << "Total Processes: " << totalProcesses << endl;
 
-    //---------CPU Utilizations---------
+        int runningProcesses = LinuxParser::RunningProcesses();
+        cout << "Running Processes: " << runningProcesses << endl;
+
+        long upTime = LinuxParser::UpTime();
+        cout << "Up Time: " << upTime << endl;
+
+        cout << endl;
+    }
+
+    cin.get();
+    cin.get();
+}
+
+void displayCpuUtilizations() {
+    cout << "\t\t\t -- CPU Utilizations --\n\n";
+
     for (int i = 0 ; i < 10 ; i++, sleep(1)) {
         vector<float> cpuUtilizations = Processor::Utilizations();
         for (int i = 0 ; i < cpuUtilizations.size() ; i++) {
-            cout << "CPU core " << i - 1 << ": " << cpuUtilizations[i] * 100.0 << "%" << endl;
+            cout << "Core " << i - 1 << ": " << cpuUtilizations[i] * 100.0 << "%" << endl;
 
         } 
+
+        cout << endl;
     }
+
+    cin.get();
+    cin.get();
+}
+
+void systemDataSelect() {
+    int intChoice = 0;
+    char charChoice[3];
+
+    do {
+        system("clear");
+        cout << "\n";
+        cout << "\t\t\t -- Select System Data -- \n\n";
+        cout << "1. Static data\n";
+        cout << "2. Dynamic data\n";
+        cout << "3. CPU Utilization\n";
+        cout << "\n\n0. Exit\n";
+
+        cout << "Enter Option: ";
+        cin >> ("%s", charChoice);
+        stringstream tmpcnvt (charChoice);
+        tmpcnvt >> intChoice;
+
+        system("clear");
+        switch (intChoice){
+            case 1:
+                displayStaticData();
+                break;
+            case 2:
+                displayDynamicData();
+                break;
+            case 3:
+                displayCpuUtilizations();
+                break;
+            case 0:
+                exit(1);
+                break;
+            default:
+                cout << "Invalid Choice - Re-Enter / Exit";
+                break;
+        }
+
+
+    } while (intChoice != 0);
+
+}
+
+int main() {
+
+    systemDataSelect();
+    
 }
