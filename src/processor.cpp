197e29e3b7799c0ac88503d94c01de058b56e167
huang-wt <314936302@qq.com>
2023-03-04 03:25:55 +0000

    refine and document processor

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,58 +1,73 @@
 #include "processor.h"
-#include "linux_parser.h"
 
-long Processor::jiffies(int cid) {
-    vector<string> jiffies = LinuxParser::cpuTimes(cid);
-    long t_jiffies = 0;
-    for(string jiffie : jiffies) {
-        t_jiffies += std::stoi(jiffie);
-    }
-    return t_jiffies;
+#include "system_parser.h"
+#include "command.h"
+
+int Processor::PhysicalCores() {
+    return physical_cores;
 }
 
-long Processor::idleJiffies(int cid) {
-    vector<string> jiffies = LinuxParser::cpuTimes(cid);
-    long i_jiffies = 0;
-    long idle = std::stoi(jiffies[3]);
-    long iowait = std::stoi(jiffies[4]);
-    i_jiffies = idle + iowait;
-    return i_jiffies;
+int Processor::LogicalCores() {
+    return logical_cores;
 }
 
-long Processor::activeJiffies(int cid) {
-    long a_jiffies = 0;
-    a_jiffies = jiffies(cid) - idleJiffies(cid);
-    return a_jiffies;
+int Processor::HyperThreadedCores() {
+    return hyperthreaded_cores;
 }
 
-// Return the aggregate CPU utilization
-vector<float> Processor::utilizations() {
-    vector<float> cpuUtilizations;
-    long currActiveJiffies;
-    long currTotalJiffies;
-    bool isFirstTime = prevActiveJiffies.empty();
+int Processor::ECores() {
+    return e_cores;
+}
 
-    for (int cid = -1 ; cid < logicalCores ; cid++) { 
-        currActiveJiffies = activeJiffies(cid);
-        currTotalJiffies = jiffies(cid);
+int Processor::PCores() {
+    return p_cores;
+}
 
-        if (!isFirstTime) {
-            cpuUtilizations.push_back((float) (currActiveJiffies - prevActiveJiffies[cid + 1]) /
-                                                (currTotalJiffies - prevTotalJiffies[cid + 1]));
-            prevActiveJiffies[cid + 1] = currActiveJiffies;
-            prevTotalJiffies[cid + 1] = currTotalJiffies;
+std::vector<float> Processor::Utilisations() {
+    return utilisations;
+}
+
+int Processor::Temperature() {
+    return temperature;
+}
+
+void Processor::UpdateUtilisations() {
+    long curr_active_jiffies;
+    long curr_total_jiffies;
+    bool is_first_time = prev_active_jiffies.empty();
+
+    for (int cid = -1 ; cid < LogicalCores() ; cid++) { 
+        curr_active_jiffies = SystemParser::ActiveJiffiesC(cid);
+        curr_total_jiffies = SystemParser::TotalJiffies(cid);
+
+        if (!is_first_time) {
+            utilisations[cid + 1] = (float) (curr_active_jiffies - prev_active_jiffies[cid + 1]) /
+                                            (curr_total_jiffies - prev_total_jiffies[cid + 1]);
+            prev_active_jiffies[cid + 1] = curr_active_jiffies;
+            prev_total_jiffies[cid + 1] = curr_total_jiffies;
         } else {
-            cpuUtilizations.push_back(0);
-            prevActiveJiffies.push_back(currActiveJiffies);
-            prevTotalJiffies.push_back(currTotalJiffies);
+            utilisations[cid + 1] = 0;
+            prev_active_jiffies.push_back(curr_active_jiffies);
+            prev_total_jiffies.push_back(curr_total_jiffies);
         }
     }
-
-    return cpuUtilizations;
 }
 
-int Processor::getCpuTemp() {
-    string cmd = "cat /sys/class/thermal/thermal_zone0/temp";
-    int temp = stoi(Command::exec(cmd).output) / 1000;
-    return temp;
+void Processor::UpdateTemperature() {
+    std::string cmd = "cat /sys/class/thermal/thermal_zone0/temp";
+    temperature = stoi(raymii::Command::exec(cmd).output) / 1000;
+}
+
+Processor::Processor() {
+    // Initialise cpu cores number of the following types
+    physical_cores = std::stoi(raymii::Command::exec("lscpu -b -p=Core,Socket | grep -v '^#' | sort -u | wc -l").output);
+	logical_cores = std::stoi(raymii::Command::exec("lscpu -b -p=cpu | grep -v '^#' | sort -u | wc -l").output);
+	hyperthreaded_cores = (logical_cores - physical_cores) * 2;
+	e_cores = logical_cores - hyperthreaded_cores;
+    p_cores = hyperthreaded_cores / 2;
+
+    // Initialise utilisations with all 0 in length of the number of logical cores + 1
+    for (int i = 0 ; i < logical_cores + 1 ; i++) {
+        utilisations.push_back(0);
+    }
 }
--- src/processor.h
+++ src/processor.h
@@ -1,29 +1,81 @@
 #ifndef PROCESSOR_H
 #define PROCESSOR_H
 
-#include <string>
 #include <vector>
-#include "command.h"
-
-using namespace std;
-using namespace raymii;
 
 class Processor {
+
   	public:
-		int physCores = stoi(Command::exec("lscpu -b -p=Core,Socket | grep -v '^#' | sort -u | wc -l").output);
-		int logicalCores = stoi(Command::exec("lscpu -b -p=cpu | grep -v '^#' | sort -u | wc -l").output);
-		int hyperThreadedCores = (logicalCores - physCores)*2;
-		int eCores = logicalCores - hyperThreadedCores;
-		int pCores = hyperThreadedCores / 2;
-		int getCpuTemp();
-		vector<float> utilizations();
-		long jiffies(int cid);
+		/**
+         * Getter method for physical_cores.
+         * @return The number of physical cores of the processor.
+        */
+		int PhysicalCores();
+
+		/**
+         * Getter method for logical_cores.
+         * @return The number of logical cores of the processor.
+        */
+		int LogicalCores();
+
+		/**
+         * Getter method for hyperthreaded_cores.
+         * @return The number of hyperthreaded cores of the processor.
+        */
+		int HyperThreadedCores();
+
+		/**
+         * Getter method for e_cores.
+         * @return The number of efficiency cores of the processor.
+        */
+		int ECores();
+
+		/**
+         * Getter method for e_cores.
+         * @return The number of performance cores of the processor.
+        */
+		int PCores();
+		
+		/**
+         * Getter method for cpu_utilisations.
+         * @return The vector containing the utilisation in percentage of each cpu core.
+        */
+		std::vector<float> Utilisations();
+
+		/**
+         * Getter method for temperature.
+         * @return The cpu temperature in degree Celsius.
+        */
+		int Temperature();
+		
+		/**
+		 * Update the utilisation of each cpu core and overall usage in percentage.
+		*/
+		void UpdateUtilisations();
+
+		/**
+		 * Update the cpu temperature.
+		*/
+		void UpdateTemperature();
+
+		Processor();
     
   	private:
-		vector<long> prevActiveJiffies;
-		vector<long> prevTotalJiffies;
-		long idleJiffies(int cid);
-		long activeJiffies(int cid);
+		int physical_cores;
+		int logical_cores;
+		int hyperthreaded_cores;
+		int e_cores;
+		int p_cores;
+		int temperature;
+
+		// First element represents general/overall cpu utilisation,
+		// followed by that of all cores with index cid-1
+		std::vector<float> utilisations;
+
+		// Record previous cpu active/total jiffies to calculate
+		// the live utilisation of each core in a short period
+		std::vector<long> prev_active_jiffies;
+		std::vector<long> prev_total_jiffies;
 };
 
 #endif
