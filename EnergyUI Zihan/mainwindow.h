4c52615a0c67b1e2b6ce497c6710690d29b4c117
puresb <72115526+puresb@users.noreply.github.com>
2023-03-13 09:00:42 +0000

    Add files via upload

---
+++ EnergyUI Zihan/mainwindow.h
@@ -0,0 +1,33 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QTreeWidgetItem>
+#include <QFile>
+#include <QTextStream>
+#include <QInputDialog>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+    void setWindowStyle();
+
+private slots:
+    void on_treeWidget_currentItemChanged(QTreeWidgetItem *current, QTreeWidgetItem *previous);
+
+    void on_pushButton_edit_clicked();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
